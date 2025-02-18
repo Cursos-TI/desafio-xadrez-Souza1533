75b7657d54a57ebc8eff834fd6394bf6aad2424e
Nicolas Souza <143300017+Souza1533@users.noreply.github.com>
2025-02-18 03:11:19 +0000

    implementado peça cavalo usando loop aninhado, melhoria no menu de opçes, adcionado escolha de lado para as peças.

--- xadrez.c
+++ xadrez.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    int peca, ladoL, i, movcompleto = 1;
+    int peca, lado, i, movcompleto = 1;
 
     printf("Jogo de xadrez\n");
     printf("\nEscolha qual peça quer movimentar");
@@ -14,36 +14,74 @@ int main() {
     printf("\n5. SAIR do programa\n\n");
     scanf("%d", &peca);
 
-    switch (peca)
-    {
-    case 1:
-        for (i = 1; i <= 5; i++){
-            printf("\nBispo: %d casa(s) na diagonal superior direita\n", i);
-        } 
-        printf("\nBispo foi movimentado.");
-        break;
+    switch (peca){
 
-    case 2:
-        for (i = 1; i <= 5; i++){
-            printf("Torre: %d casa(s) para direita\n", i);
-        } 
-        printf("\nTorre foi movimentado.");
-        break;
+    case 1://BISPO
+    printf("\nPara que lado fazer movimento diagonal");
+    printf("\n1. Direita");
+    printf("\n2. Esquerda\n");
+    scanf("%d", &lado);
 
-    case 3:
-        for (i = 1; i <= 8; i++){
-            printf("\nRainha: %d casa(s) para a esquerda\n", i);
+        switch(lado) {
+        case 1:   //DIREITA
+            for (i = 1; i <= 5; i++){
+                printf("\nBispo: %d casa(s) na diagonal superior Direita\n", i);
+            } 
+            printf("\nBispo foi movimentado."); break;
+
+        case 2:    //ESQUERDA
+            for (i = 1; i <= 5; i++){
+                printf("\nBispo: %d casa(s) na diagonal superior Esquerda\n", i);
+            } 
+            printf("\nBispo foi movimentado."); break;
+        } break;//----------
+
+    case 2://TORRE
+    printf("\nPara que lado fazer movimentos");
+    printf("\n1. Direita");
+    printf("\n2. Esquerda\n");
+    scanf("%d", &lado);
+
+        switch(lado) {
+        case 1:
+            for (i = 1; i <= 5; i++){
+                printf("Torre: %d casa(s) para Direita\n", i);
+            } 
+            printf("\nTorre foi movimentado."); break;
+
+        case 2:
+        for (i = 1; i <= 5; i++){
+            printf("Torre: %d casa(s) para Esquerda\n", i);
         } 
-        printf("\nRainha foi movimentado.");
-        break;
+        printf("\nTorre foi movimentado."); break;
+    } break;//------------
+
+    case 3://RAINHA
+    printf("\nPara que lado fazer movimentos");
+    printf("\n1. Direita");
+    printf("\n2. Esquerda\n");
+    scanf("%d", &lado);
+
+        switch(lado) {
+        case 1:
+            for (i = 1; i <= 8; i++){
+                printf("\nRainha: %d casa(s) para a Direita\n", i);
+            } 
+            printf("\nRainha foi movimentado."); break;
+        case 2:
+            for (i = 1; i <= 8; i++){
+                printf("\nRainha: %d casa(s) para a Esquerda\n", i);
+            } 
+            printf("\nRainha foi movimentado."); break;
+        } break;//---------------------
 
     case 4://opcao Cavalo / escolher o lado para movimento
     printf("\nPara que lado fazer movimento em L");
     printf("\n1. Direita");
     printf("\n2. Esquerda\n");
-    scanf("%d", &ladoL);
+    scanf("%d", &lado);
 
-        switch(ladoL) {
+        switch(lado) {
 
         case 1://caso foir direita
         printf("\nMovimento em L Direita...\n");
